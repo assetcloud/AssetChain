6e82f7227a15b01163b48b5cd52da0083b709547
linj-disanbo <linj@disanbo.com>
2022-05-13 15:59:50 +0800

    fix crash if miner no fee 8e0e84848b5b9bf2f463c7f9422c555582cc5d8c

--- plugin/consensus/pos33/node.go
+++ plugin/consensus/pos33/node.go
@@ -21,7 +21,6 @@ import (
 
 var plog = log15.New("module", "pos33")
 
-
 // 区块制作人
 type maker struct {
 	my       *pt.Pos33SortMsg              // 我的抽签
@@ -511,7 +510,8 @@ func (n *node) blockCheck(b *types.Block) error {
 	}
 
 	act, err := getMiner(b)
-	if err != nil {
+	if act == nil || err != nil {
+		plog.Error("block check getMiner error", "err", err, "height", b.Height)
 		return err
 	}
 	if act.Sort == nil || act.Sort.Proof == nil || act.Sort.Proof.Input == nil {
